d98aa7abeb782f63140949cfea52e2593cbf34b2
joao <joaopedro37@gmail.com>
2021-10-18 17:27:46 -0300

    reactJs iniciado

---
+++ React_js/primeiro_app/src/App.js
@@ -0,0 +1,15 @@
+
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        
+        <h1>oiii, tudo bem??</h1>
+      </header>
+    </div>
+  );
+}
+
+export default App;
