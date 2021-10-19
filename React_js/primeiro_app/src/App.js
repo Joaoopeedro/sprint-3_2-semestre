5fea914f850f9e9a30971c170192d59e71b664a1
joao <joaopedro37@gmail.com>
2021-10-19 17:04:00 -0300

    Relogio feito ...

--- React_js/primeiro_app/src/App.js
+++ React_js/primeiro_app/src/App.js
@@ -1,12 +1,78 @@
 
+import react from 'react';
 import './App.css';
 
+function DataFormatada(props) {
+  return <h2>Horario atual : {props.date.toLocaleTimeString()}</h2>
+}
+
+//Componente de classe
+class Clock extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //Define a propiedade date pagdn a hora e data atual
+      date: new Date()
+    }
+  }
+
+  //Ciclo de vida  que ocorre quando o componente clock é inserido na arvore DOM, ou seja , ciclo de vida de nascimento
+  componentDidMount() {
+    this.TimerID = setInterval(() => {
+      this.thick()
+    }, 1000)
+  };
+
+  // Ciclo de vida que ocorre quando o componente Clock é removido da arvore DOM 
+  componentWillUnmount() {
+    clearInterval(this.TimerID)
+
+  };
+
+  thick() {
+    this.setState({
+      date: new Date()
+    })
+  };
+
+  Pausar(){
+    this.componentWillUnmount()
+    console.log('Relógio ' + this.TimerID + ' está pausado')
+  }
+
+  Retomar(){
+    this.TimerID = setInterval(() => {
+      this.thick()
+    }, 1000)
+    console.log('Relógio retomado')
+    console.log('Agora sou o relogio '+ this.TimerID)
+  }
+  
+ 
+
+  //Renderiza o conteudo do retorno para a tela 
+  render() {
+    return (
+      <div>
+        <h1>Relógio</h1>
+        <DataFormatada date={this.state.date} />
+        <button className="bnt_1" onClick={() => this.Pausar()}>Pausar</button>
+        <button className="bnt_2" onClick={() => this.Retomar()}>Retomar</button>
+        
+      </div>
+    )
+  }
+}
+
+
+
+// Componente princiapl
 function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
-        <h1>oiii, tudo bem??</h1>
+        <Clock />
+        <Clock />
       </header>
     </div>
   );
