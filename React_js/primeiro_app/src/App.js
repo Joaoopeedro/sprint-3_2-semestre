6afc300d7407b5d15136db60817f6554f620d872
joao <joaopedro37@gmail.com>
2021-10-19 17:40:08 -0300

    Relogio feito ...

--- React_js/primeiro_app/src/App.js
+++ React_js/primeiro_app/src/App.js
@@ -64,6 +64,62 @@ class Clock extends react.Component {
   }
 }
 
+//Componente de classe
+class Clock1 extends react.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      //Define a propiedade date pagdn a hora e data atual
+      date: new Date()
+    }
+  }
+
+  //Ciclo de vida  que ocorre quando o componente clock é inserido na arvore DOM, ou seja , ciclo de vida de nascimento
+  componentDidMount() {
+    this.TimerID = setInterval(() => {
+      this.thick()
+    }, 1000)
+  };
+
+  // Ciclo de vida que ocorre quando o componente Clock é removido da arvore DOM 
+  componentWillUnmount() {
+    clearInterval(this.TimerID)
+
+  };
+
+  thick() {
+    this.setState({
+      date: new Date()
+    })
+  };
+
+  Pausar(){
+    this.componentWillUnmount()
+    console.log('Relógio ' + this.TimerID + ' está pausado')
+  }
+
+  Retomar(){
+    this.TimerID = setInterval(() => {
+      this.thick()
+    }, 1000)
+    console.log('Relógio retomado')
+    console.log('Agora sou o relogio '+ this.TimerID)
+  }
+  
+ 
+
+  //Renderiza o conteudo do retorno para a tela 
+  render() {
+    return (
+      <div>
+        <h1>Relógio infinito</h1>
+        <DataFormatada date={this.state.date} />
+        
+        
+      </div>
+    )
+  }
+}
 
 
 // Componente princiapl
@@ -72,7 +128,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <Clock />
-        <Clock />
+        <Clock1 />
       </header>
     </div>
   );
